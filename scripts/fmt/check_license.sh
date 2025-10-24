98e9418a841cb7e1cc376e3c580d3c1a3c99b97d
Dmitriy Shilin <dshil@fastmail.com>
2025-10-24 12:46:50 +0200

    Verify license header during CI

--- scripts/fmt/check_license.sh
+++ scripts/fmt/check_license.sh
@@ -2,6 +2,16 @@
 
 set -xe
 
+if [[ -z "$PROJECT_PATH" ]]; then
+    echo "Error: PROJECT_PATH is missed or empty"
+    exit 1
+fi
+
+if [[ -z "$CI_COMPONENTS_PATH" ]]; then
+    echo "Error: CI_COMPONENTS_PATH is missed or empty"
+    exit 1
+fi
+
 output=$("$CI_COMPONENTS_PATH"/scripts/fmt/verify_license.py --path "$PROJECT_PATH" 2>&1 || exit 1)
 if [ -n "$output" ]; then
   echo "Error: Some files are missing the Apache 2.0 license header:"
