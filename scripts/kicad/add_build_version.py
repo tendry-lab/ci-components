80c700ab5794d22e73a61531468f1d3e7e878065
Dmitriy Shilin <dshil@fastmail.com>
2026-06-29 11:55:22 +0200

    kicad: allow to specify company name for PCB version adding

--- scripts/kicad/add_build_version.py
+++ scripts/kicad/add_build_version.py
@@ -14,16 +14,17 @@ if __name__ == "__main__":
         sys.exit(1)
 
     board_path = sys.argv[1]
+    company_id = os.environ.get("COMPANY_ID", "Tendry Lab")
     project_id = os.environ.get("PROJECT_ID", "none")
     build_prefix = os.environ.get("BUILD_PREFIX", "0.0.0")
     build_number = os.environ.get("BUILD_NUMBER", "0")
 
     board = pcbnew.LoadBoard(board_path)
 
-    new_text = f"Tendry Lab\n{project_id}\n{build_prefix}\n{build_number}"
+    new_text = f"{company_id}\n{project_id}\n{build_prefix}\n{build_number}"
 
     for item in board.GetDrawings():
-        if isinstance(item, pcbnew.PCB_TEXT) and item.GetText().startswith("Tendry Lab"):
+        if isinstance(item, pcbnew.PCB_TEXT) and item.GetText().startswith(company_id):
             item.SetText(new_text)
 
     pcbnew.SaveBoard(board_path, board)
